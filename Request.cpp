f61e8c9883677b3bd658f43b40d58251142d1e41
grandiloquent <1785907865@qq.com>
2019-07-19 14:10:59 +0800

    changed

--- Request.cpp
+++ Request.cpp
@@ -3,31 +3,37 @@
 #include "Request.h"
 #include "httplib.h"
 
-Request::Request() {
-
+Request::Request()
+{
 }
 
-Request::~Request() {
-
+Request::~Request()
+{
 }
-std::string Request::Touch(const char *host, const char *path, Request::Method method) const {
-  std::shared_ptr<httplib::Response> res{0};
-  httplib::Client client(host);
-  if (method == Request::Method::GET) {
+std::string Request::Touch(const char* host, const char* path, Request::Method method) const
+{
+    std::shared_ptr<httplib::Response> res{ 0 };
+    httplib::Client client(host);
+    if (method == Request::Method::GET)
+    {
 
-    res = client.Get(host);
-  }
+        res = client.Get(host);
+    }
+    else if (method == Request::Method::POST)
+    {
+        //res = client.Post(host);
+    }
 #ifndef DEBUG
-  std::cout << method
-            << " " << host
-            << " " << path
-            << std::endl;
+    std::cout << method
+              << " " << host
+              << " " << path
+              << std::endl;
 #endif
-  if (res) {
-    std::cout << res->status << std::endl;
-    return res->body;
-  }
-  std::cout << "res is null" << std::endl;
-  return "";
-
+    if (res)
+    {
+        std::cout << res->status << std::endl;
+        return res->body;
+    }
+    std::cout << "res is null" << std::endl;
+    return "";
 }
